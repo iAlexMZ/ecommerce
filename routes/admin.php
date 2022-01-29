906190b4724fcf66d748ac8efbe436d9e02a5cd4
iAlexMZ <a.martinezzamora01@gmail.com>
2022-01-29 22:51:08 +0000

    Fin video 125

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Admin\ShowProducts;
+use App\Http\Livewire\Admin\CreateProduct;
 
 
 Route::get('/', ShowProducts::class)->name('admin.index');
@@ -9,5 +10,4 @@ Route::get('/', ShowProducts::class)->name('admin.index');
 Route::get('products/{product}/edit', function () {
 })->name('admin.products.edit');
 
-Route::get('products/create', function () {
-})->name('admin.products.create');
+Route::get('products/create', CreateProduct::class)->name('admin.products.create');
