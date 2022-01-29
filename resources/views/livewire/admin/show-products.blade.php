91fb049caae7697c26e81f2ada959525bf45aa17
iAlexMZ <a.martinezzamora01@gmail.com>
2022-01-29 22:26:20 +0000

    Fin video 122

--- resources/views/livewire/admin/show-products.blade.php
+++ resources/views/livewire/admin/show-products.blade.php
@@ -4,4 +4,47 @@
             Lista de productos
         </h2>
     </x-slot>
+
+    <x-table-responsive>
+        <div class="px-6 py-4">
+            <x-jet-input class="w-full" wire:model="search" type="text"
+                placeholder="Introduzca el nombre del producto a buscar" />
+        </div>
+
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Title
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Role
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach ($products as $product)
+                    <tr>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="px-6 py-4">
+            {{ $products->links() }}
+        </div>
+    </x-table-responsive>
 </div>
