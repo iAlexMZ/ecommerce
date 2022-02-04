cfeb6df1dceb7d5c6d874d7fb6200594cbf55e40
iAlexMZ <a.martinezzamora01@gmail.com>
2022-02-04 19:11:24 +0000

    Fin video 159

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -4,9 +4,35 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Brand;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use WithFileUploads;
 
 class CreateCategory extends Component
 {
+    protected $rules = [
+        'createForm.name' => 'required',
+        'createForm.slug' => 'required|unique:categories,slug',
+        'createForm.icon' => 'required',
+        'createForm.image' => 'required|image|max:1024',
+        'createForm.brands' => 'required',
+    ];
+
+    protected $validationAttributes = [
+        'createForm.name' => 'nombre',
+        'createForm.slug' => 'slug',
+        'createForm.icon' => 'icono',
+        'createForm.image' => 'imagen',
+        'createForm.brands' => 'marcas',
+    ];
+
+    public $createForm = [
+        'name' => null,
+        'slug' => null,
+        'icon' => null,
+        'image' => null,
+        'brands' => [],
+    ];
+
     public $brands;
 
     public function mount()
@@ -19,8 +45,14 @@ class CreateCategory extends Component
         $this->brands = Brand::all();
     }
 
+    public function updatedCreateFormName($value)
+    {
+        $this->createForm['slug'] = Str::slug($value);
+    }
+
     public function save()
     {
+        $this->validate();
     }
 
     public function render()
